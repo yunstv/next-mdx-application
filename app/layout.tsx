3d1958977b2ab003c020a54afd6fd603979d58b8
zy <yunstv@163.com>
2024-05-21 18:46:44 +0800

    feat: update global file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import "./syntax-highlighting.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
