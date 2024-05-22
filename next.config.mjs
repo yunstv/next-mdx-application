4b0064feb62a255c8777463a72bcb79605357421
zy <yunstv@163.com>
2024-05-22 23:09:00 +0800

    feat: add next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.mjs/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
