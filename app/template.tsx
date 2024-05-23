8a134e3a80cab85f273cd596cd75f2baf382c207
zy <yunstv@163.com>
2024-05-23 23:41:31 +0800

    feat: add next-themes

--- app/template.tsx
+++ app/template.tsx
@@ -1,10 +1,21 @@
 "use client";
 import { CssLibPreferenceProvider } from "@/lib/context";
-
+import { ThemeProvider } from "next-themes";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <CssLibPreferenceProvider>{children}</CssLibPreferenceProvider>;
+  return (
+    <CssLibPreferenceProvider>
+      <ThemeProvider
+        disableTransitionOnChange
+        attribute="class"
+        value={{ light: "light-theme", dark: "dark-theme" }}
+        defaultTheme="system"
+      >
+        {children}
+      </ThemeProvider>
+    </CssLibPreferenceProvider>
+  );
 }
